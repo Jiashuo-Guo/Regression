5a8f59d1ad085b676b144e63f9c67a98ff120ac9
lhan <liangyue.han@ecl21.ec-lyon.fr>
2022-10-20 17:28:52 +0800

    v5
    acp donnee

--- affichage.py
+++ affichage.py
@@ -1,6 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def acp(X):
+    
+    # Calcul de la matrice M (métrique)
+    M = np.eye(X.shape[1])
+    
+    # Calcul de la matrice D (poid des individus)
+    D = np.eye(X.shape[0]) / X.shape[0]
+    
+    # Calcul de la matrice de covariance pour les individus
+    Xcov_ind = X.T.dot(D.dot(X.dot(M)))
+    
+    # Calcul des valeurs et vecteurs propres de la matrice de covariance
+    L,U = np.linalg.eig(Xcov_ind)
+    
+    # Tri par ordre décroissant des valeurs des valeurs propres
+    indices = np.argsort(L)[::-1]
+    val_p_ind = np.sort(L)[::-1]
+    vect_p_ind = U[:,indices]
+    
+    # Calcul des facteurs pour les individus
+    fact_ind = X.dot(M.dot(vect_p_ind))
+    return fact_ind 
 
 def affichage(X, Y):
     """ Affichage en 2 dimensions des données et de la courbe de régression linéaire déterminée par theta
@@ -19,6 +41,7 @@ def affichage(X, Y):
     None
 
     """
+    X = acp(X)
     N = X.shape[0]
     plt.figure()
     apparance = ['bx','ro','g,','y<','kp']
--- decoupage_donnees.py
+++ decoupage_donnees.py
@@ -2,29 +2,9 @@ import numpy as np
 from scipy.special import softmax
 
 def decoupage_donnees(X,Y,R,prop_test=0.5):
-    """ Découpe les données initiales en trois sous-ensembles distincts d'apprentissage, de validation et de test
+     # Découpe les données initiales en DEUX sous-ensembles distincts d'apprentissage Det de test
     
-    Parametres
-    ----------
-    x : matrice des données de dimension [N, nb_var]
-    d : matrice des valeurs cibles [N, nb_cible]
-    prop_val : proportion des données de validation sur l'ensemble des données (entre 0 et 1)
-    prop_test : proportion des données de test sur l'ensemble des données (entre 0 et 1)
     
-    avec N : nombre d'éléments, nb_var : nombre de variables prédictives, nb_cible : nombre de variables cibles
-
-    Retour
-    -------
-    x_app : matrice des données d'apprentissage
-    d_app : matrice des valeurs cibles d'apprentissage
-    
-    x_test : matrice des données d'apprentissage
-    d_test : matrice des valeurs cibles d'apprentissage
-
-    """
-    #######################
-    ##### A compléter ##### 
-    #######################
     N = X.shape[0]
     nb_var = X.shape[1]
     X_app = X[:int((1-prop_test)*N),:]
@@ -33,12 +13,6 @@ def decoupage_donnees(X,Y,R,prop_test=0.5):
    
     X_test = X[int((1-prop_test)*N):,:]
     Y_test = Y[int((1-prop_test)*N):,:]
-
-    
-
-    
-    
-    
 
     return X_app, Y_app, R_app, X_test,Y_test
     
