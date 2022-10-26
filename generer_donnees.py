0f9a0bccf5569d6651500f70a2e3e78565da9d4f
lhan <liangyue.han@ecl21.ec-lyon.fr>
2022-10-27 02:45:53 +0800

    v7
    final final

--- generer_donnees.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-if __name__ == '__main__':
-    data = np.random.randint(1,100,size = (50,10))
-    l = np.random.randint(1,5,size = (50,1))
-    data = np.hstack((data,l))
-
-    np.savetxt('donnees.txt',data,delimiter = ',')
